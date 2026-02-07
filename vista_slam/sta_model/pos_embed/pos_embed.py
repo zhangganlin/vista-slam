befeaa4ee8bc2a0ffd0f28a37b6724d2bff4b2d7
Ganlin Zhang <zgl0315@gmail.com>
2026-02-07 22:35:28 +0100

    fix bug: add support for -1 positional embedding for python version rope

--- vista_slam/sta_model/pos_embed/pos_embed.py
+++ vista_slam/sta_model/pos_embed/pos_embed.py
@@ -116,28 +116,21 @@ except ImportError as e:
             super().__init__()
             self.base = freq 
             self.F0 = F0
-            self.cache = {}
-
-        def get_cos_sin(self, D, seq_len, device, dtype):
-            if (D,seq_len,device,dtype) not in self.cache:
-                inv_freq = 1.0 / (self.base ** (torch.arange(0, D, 2).float().to(device) / D))
-                t = torch.arange(seq_len, device=device, dtype=inv_freq.dtype)
-                freqs = torch.einsum("i,j->ij", t, inv_freq).to(dtype)
-                freqs = torch.cat((freqs, freqs), dim=-1)
-                cos = freqs.cos() # (Seq, Dim)
-                sin = freqs.sin()
-                self.cache[D,seq_len,device,dtype] = (cos,sin)
-            return self.cache[D,seq_len,device,dtype]
             
         @staticmethod
         def rotate_half(x):
             x1, x2 = x[..., : x.shape[-1] // 2], x[..., x.shape[-1] // 2 :]
             return torch.cat((-x2, x1), dim=-1)
             
-        def apply_rope1d(self, tokens, pos1d, cos, sin):
-            assert pos1d.ndim==2
-            cos = torch.nn.functional.embedding(pos1d, cos)[:, None, :, :]
-            sin = torch.nn.functional.embedding(pos1d, sin)[:, None, :, :]
+        def apply_rope1d(self, tokens, pos1d):
+            assert pos1d.ndim == 2
+            q = tokens.size(-1) // 2
+            inv_freq = self.F0 / (self.base ** (torch.arange(q, device=tokens.device, dtype=tokens.dtype) / q))
+            freqs = pos1d.to(tokens.dtype).unsqueeze(-1) * inv_freq
+            cos = freqs.cos().unsqueeze(1)
+            sin = freqs.sin().unsqueeze(1)
+            cos = torch.cat((cos, cos), dim=-1)
+            sin = torch.cat((sin, sin), dim=-1)
             return (tokens * cos) + (self.rotate_half(tokens) * sin)
             
         def forward(self, tokens, positions):
@@ -151,10 +144,9 @@ except ImportError as e:
             assert tokens.size(3)%2==0, "number of dimensions should be a multiple of two"
             D = tokens.size(3) // 2
             assert positions.ndim==3 and positions.shape[-1] == 2 # Batch, Seq, 2
-            cos, sin = self.get_cos_sin(D, int(positions.max())+1, tokens.device, tokens.dtype)
             # split features into two along the feature dimension, and apply rope1d on each half
             y, x = tokens.chunk(2, dim=-1)
-            y = self.apply_rope1d(y, positions[:,:,0], cos, sin)
-            x = self.apply_rope1d(x, positions[:,:,1], cos, sin)
+            y = self.apply_rope1d(y, positions[:, :, 0])
+            x = self.apply_rope1d(x, positions[:, :, 1])
             tokens = torch.cat((y, x), dim=-1)
             return tokens
